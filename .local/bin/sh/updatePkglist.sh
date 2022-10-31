5f0a478e5bc1ecd63a42ed1a4eeef333099c6550
w <w@dunkelheit.biz>
2022-10-31 12:33:56 +0000

    packages

--- .local/bin/sh/updatePkglist.sh
+++ .local/bin/sh/updatePkglist.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ## Make a list of all explicitly installed packages (non-AUR)
-pacman -Qqen > "$HOME/.local/pkglist && notify-send" "Updated package list." "$HOME/.local/pkglist updated."
+pacman -Qqen > "$HOME/.local/pkglist" && notify-send "Updated package list." "$HOME/.local/pkglist updated."
 
 
 ## Make a list of all explicitly installed packes (AUR / Manual)
-pacman -Qqem > "$HOME/.local/pkglist-aur && notify-send" "Updated AUR package list." "$HOME/.local/pkglist-aur updated."
+pacman -Qqem > "$HOME/.local/pkglist-aur" && notify-send "Updated AUR package list." "$HOME/.local/pkglist-aur updated."
 
