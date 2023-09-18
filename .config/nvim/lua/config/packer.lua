6bc1934d527e359d269a247a8d87fd77f90b2589
William Mack <w@dunkelheit.xyz>
2023-09-18 09:34:46 +0100

    neovim config

---
+++ .config/nvim/lua/config/packer.lua
@@ -0,0 +1,42 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- Telescope plugin
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- TreeSitter
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	-- Harpoon
+	use('theprimeagen/harpoon')
+	-- Undo Tree
+	use('mbbill/undotree')
+	-- Fugitive
+	use('tpope/vim-fugitive')
+	-- LSP
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+end)
