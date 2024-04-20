774011dbc12f5e9ad5f54fcac2adb6179c32a23a
William Mack <w@dunkelheit.xyz>
2024-04-20 09:36:00 +0100

    wayland time

---
+++ .config/waybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Kill all polybars
+while [ "$(pgrep waybar)" ]
+	do
+		killall -q waybar
+	done
+
+# Wait until kill confirmed.
+while pgrep -u $UID -x waybar >dev/null; do sleep 1; done
+
+# Launch Polybar
+waybar
+
+echo "Waybar launched..."
+
