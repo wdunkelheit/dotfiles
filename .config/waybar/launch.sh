c9b3c8b98188a645a5808b6e5d4e293ce2a3c617
William Mack <w@dunkelheit.xyz>
2024-04-24 07:21:43 +0100

    housekeeping paste fix alacritty

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -2,9 +2,9 @@
 
 # Kill all polybars
 while [ "$(pgrep waybar)" ]
-	do
-		killall -q waybar
-	done
+do
+    killall -q waybar
+done
 
 # Wait until kill confirmed.
 while pgrep -u $UID -x waybar >dev/null; do sleep 1; done
